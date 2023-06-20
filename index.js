275dfb26fe153dbeed42b29615b4722a561b2a31
Alexey Sevastynov <alexseva94@gmail.com>
2023-06-20 17:13:02 +0300

    npm i jsonwebtoken

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const PORT = 4444;
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
   //"request" from Frontend
   response.send("hello world"); // show message frontend on page
+});
+
+app.post("/auth/login", (req, res) => {
+  console.log(req.body);
+
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      fullName: "Alexey Sevastnov",
+    },
+    "secretKey"
+  );
+
+  res.json({ success: true, token });
 });
 
 // how start the server
